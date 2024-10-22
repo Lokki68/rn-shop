33abe8db5a152720d7ed641b80bfa987ef227201
LIBERT Brice <bricel67@gmail.com>
2024-10-22 17:28:19 +0200

    commit

---
+++ src/app/(shop)/index.tsx
@@ -0,0 +1,35 @@
+import { View, Text, FlatList, StyleSheet } from 'react-native'
+import { PRODUCTS } from '../../../assets/products'
+import ProductListItem from '../../components/product-list-item'
+import ListHeader from '../../components/list-header'
+
+export default function Home() {
+  return (
+    <View>
+      <FlatList 
+        data={PRODUCTS} renderItem={({item}) => (
+          <ProductListItem product={item}/>
+        )} 
+        keyExtractor={item => item.id.toString()} 
+        numColumns={2}
+        ListHeaderComponent={ListHeader}
+        contentContainerStyle={styles.flatlistContent}
+        columnWrapperStyle={styles.flatlistColumn}
+        style={styles.flatlist}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  flatlist: {
+    paddingHorizontal: 10,
+    paddingVertical: 5
+  },
+  flatlistContent: {
+    paddingBottom: 20,
+  },
+  flatlistColumn: {
+    justifyContent: 'space-between'
+  }
+})
